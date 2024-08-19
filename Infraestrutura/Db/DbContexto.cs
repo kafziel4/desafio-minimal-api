c2079d8b82e8f56e08e0dc26bb7e0b09aca63c6c
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-18 23:23:49 -0300

    feat: adiciona seed do administrador padrão

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -11,4 +11,16 @@ public class DbContexto : DbContext
         : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = 1,
+                Email = "administrador@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            });
+    }
 }
