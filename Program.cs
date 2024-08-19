23f58742ba076b30ababa3ab6f966a8a62b4995e
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-18 22:44:00 -0300

    feat: adiciona login em memória

--- Program.cs
+++ Program.cs
@@ -3,4 +3,15 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDto loginDto) => 
+    loginDto is { Email: "adm@test.com", Senha: "123456" }
+        ? Results.Ok("Login com sucesso")
+        : Results.Unauthorized());
+
 app.Run();
+
+public class LoginDto
+{
+    public required string Email { get; set; }
+    public required string Senha { get; set; }
+}
