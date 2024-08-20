9a1df21a27ce80a991ae275e205f72342d7a9c3f
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-19 23:09:56 -0300

    feat: adiciona resposta JSON na rota Home

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,7 @@
+﻿namespace MinimalApi.Dominio.ModelViews;
+
+public struct Home
+{
+    public string Mensagem => "Bem-vindo à API de veículos - Minimal API";
+    public string Doc => "/swagger";
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Dominio.DTOs;
 using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.ModelViews;
 using MinimalApi.Dominio.Servicos;
 using MinimalApi.Infraestrutura.Db;
 
@@ -25,7 +26,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Json(new Home()));
 
 app.MapPost("/login", ([FromBody] LoginDto loginDto, IAdministradorServico administradorServico) => 
     administradorServico.Login(loginDto) is not null
