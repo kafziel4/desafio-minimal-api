84964bd70b9d928a1acaaeff460ea3a29d0cf96c
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-21 23:05:11 -0300

    test: implementa testes de serviços

---
+++ Test/Domain/Servicos/AdministradorServicoTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.DTOs;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.Db;
+
+namespace Test.Domain.Servicos;
+
+[TestClass]
+public class AdministradorServicoTest
+{
+    private DbContexto _context = default!;
+
+    private DbContexto CriarContextoDeTeste()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")!;
+        var options = new DbContextOptionsBuilder<DbContexto>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+
+        return new DbContexto(options);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context = CriarContextoDeTeste();
+        _context.Database.Migrate();
+        _context.Database.ExecuteSqlRaw("TRUNCATE TABLE administradores");
+    }
+
+    [TestMethod]
+    public void TestandoSalvarAdministrador()
+    {
+        // Arrange
+        var adm = new Administrador
+        {
+            Id = 1,
+            Email = "teste@teste.com",
+            Senha = "teste",
+            Perfil = "Adm"
+        };
+
+        var administradorServico = new AdministradorServico(_context);
+
+        // Act
+        administradorServico.Incluir(adm);
+
+        // Assert
+        Assert.AreEqual(1, administradorServico.Todos().Count);
+    }
+
+    [TestMethod]
+    public void TestandoBuscaPorId()
+    {
+        // Arrange
+        var adm = new Administrador
+        {
+            Id = 1,
+            Email = "teste@teste.com",
+            Senha = "teste",
+            Perfil = "Adm"
+        };
+
+        var administradorServico = new AdministradorServico(_context);
+        administradorServico.Incluir(adm);
+
+        // Act
+        var admDoBanco = administradorServico.BuscaPorId(adm.Id)!;
+
+        // Assert
+        Assert.AreEqual(1, admDoBanco.Id);
+    }
+
+    [TestMethod]
+    public void TestandoLogin()
+    {
+        // Arrange
+        var adm = new Administrador
+        {
+            Id = 1,
+            Email = "teste@teste.com",
+            Senha = "teste",
+            Perfil = "Adm"
+        };
+
+        var loginDto = new LoginDto
+        {
+            Email = adm.Email,
+            Senha = adm.Senha
+        };
+
+        var administradorServico = new AdministradorServico(_context);
+        administradorServico.Incluir(adm);
+
+        // Act
+        var admDoBanco = administradorServico.Login(loginDto)!;
+
+        // Assert
+        Assert.AreEqual(1, admDoBanco.Id);
+    }
+}
---
+++ Test/Domain/Servicos/VeiculoServicoTest.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.Db;
+
+namespace Test.Domain.Servicos;
+
+[TestClass]
+public class VeiculoServicoTest
+{
+    private DbContexto _context = default!;
+
+    private DbContexto CriarContextoDeTeste()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")!;
+        var options = new DbContextOptionsBuilder<DbContexto>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+
+        return new DbContexto(options);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context = CriarContextoDeTeste();
+        _context.Database.Migrate();
+        _context.Database.ExecuteSqlRaw("TRUNCATE TABLE veiculos");
+    }
+
+    [TestMethod]
+    public void TestandoSalvarVeiculo()
+    {
+        // Arrange
+        var veiculo = new Veiculo
+        {
+            Id = 1,
+            Nome = "Fiesta",
+            Marca = "Ford",
+            Ano = 2014,
+        };
+
+        var veiculoServico = new VeiculoServico(_context);
+
+        // Act
+        veiculoServico.Incluir(veiculo);
+
+        // Assert
+        Assert.AreEqual(1, veiculoServico.Todos().Count);
+    }
+
+    [TestMethod]
+    public void TestandoBuscaPorId()
+    {
+        // Arrange
+        var veiculo = new Veiculo
+        {
+            Id = 1,
+            Nome = "Fiesta",
+            Marca = "Ford",
+            Ano = 2014,
+        };
+
+        var veiculoServico = new VeiculoServico(_context);
+        veiculoServico.Incluir(veiculo);
+
+        // Act
+        var veiculoDoBanco = veiculoServico.BuscaPorId(veiculo.Id)!;
+
+        // Assert
+        Assert.AreEqual(1, veiculoDoBanco.Id);
+    }
+
+    [TestMethod]
+    public void TestandoAtualizar()
+    {
+        // Arrange
+        var veiculo = new Veiculo
+        {
+            Id = 1,
+            Nome = "Fiesta",
+            Marca = "Ford",
+            Ano = 2014,
+        };
+
+        var veiculoServico = new VeiculoServico(_context);
+        veiculoServico.Incluir(veiculo);
+        var veiculoDoBanco = veiculoServico.BuscaPorId(veiculo.Id)!;
+
+        veiculoDoBanco.Nome = "Fiesta Sedan";
+
+        // Act
+        veiculoServico.Atualizar(veiculoDoBanco);
+
+        // Assert
+        Assert.AreEqual("Fiesta Sedan", veiculoServico.BuscaPorId(veiculo.Id)!.Nome);
+    }
+
+    [TestMethod]
+    public void TestandoApagar()
+    {
+        // Arrange
+        var veiculo = new Veiculo
+        {
+            Id = 1,
+            Nome = "Fiesta",
+            Marca = "Ford",
+            Ano = 2014,
+        };
+
+        var veiculoServico = new VeiculoServico(_context);
+        veiculoServico.Incluir(veiculo);
+        var veiculoDoBanco = veiculoServico.BuscaPorId(veiculo.Id)!;
+
+        // Act
+        veiculoServico.Apagar(veiculoDoBanco);
+
+        // Assert
+        Assert.AreEqual(null, veiculoServico.BuscaPorId(veiculo.Id));
+    }
+}
